fa09b627cca995254c23276dfde2fbde7adbb7a0
Han Gao <rabenda.cn@gmail.com>
2025-07-31 00:18:20 +0800

    sync: sync eswin code 20250630
    Signed-off-by: Han Gao <rabenda.cn@gmail.com>

--- third_party/ffmpeg/libavfilter/allfilters.c
+++ third_party/ffmpeg/libavfilter/allfilters.c
@@ -548,6 +548,8 @@ extern const AVFilter ff_vf_vstack_qsv;
 extern const AVFilter ff_vf_xstack_qsv;
 extern const AVFilter ff_vf_pad_vaapi;
 extern const AVFilter ff_vf_drawbox_vaapi;
+extern const AVFilter ff_vf_esmpp_complex;
+extern const AVFilter ff_vf_stack_esmpp;
 
 extern const AVFilter ff_vsrc_allrgb;
 extern const AVFilter ff_vsrc_allyuv;
